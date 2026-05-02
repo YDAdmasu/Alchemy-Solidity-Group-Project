61032afecf17ddc76f5add671e89b8e3784788b6
Samuel Aklilu <samuelaklilu201@gmail.com>
2026-05-02 03:08:42 +0300

    chapter-4-Inheritance

---
+++ codebases/chapter-4-Voting Smart Contract/Inheritance/ Inherit/Inheritance.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./Hero.sol";
+
+contract Mage is Hero {
+    // Inherits health and takeDamage from Hero
+}
+
+contract Warrior is Hero {
+    // Inherits health and takeDamage from Hero
+}
